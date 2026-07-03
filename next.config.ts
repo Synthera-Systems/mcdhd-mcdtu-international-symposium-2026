87267ec7daeed1d8db6a6efcbfef2cf72e761c61
Abhijit Roy <roya9435@gmail.com>
2026-07-03 19:35:28 +0530

    build: force include pg-cloudflare assets

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  serverExternalPackages: ["@prisma/client", ".prisma/client"],
+  // Instruct the Next.js file tracer to explicitly bundle pg-cloudflare binaries
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
 };
 
 export default nextConfig;
